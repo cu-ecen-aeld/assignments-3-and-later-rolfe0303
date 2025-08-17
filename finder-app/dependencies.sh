92554787ee09ed9b6e8f1fce2a2a9884024b32cb
role <rolf.eberlmora@gmail.com>
2025-08-17 19:07:53 +0200

    Added dependencies installation

--- finder-app/dependencies.sh
+++ finder-app/dependencies.sh
@@ -6,3 +6,8 @@
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y u-boot-tools
 sudo apt-get install -y qemu
+sudo apt-get install -y build-essential
+sudo apt-get install -y flex
+sudo apt-get install -y bison
+sudo apt-get install -y m4
+sudo apt-get install -y pkg-config
